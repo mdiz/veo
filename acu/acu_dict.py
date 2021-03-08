aa317f437e7ca33af7546a2f3172ee8533396b15
Mike Dismore <mike.dismore@voyantsolutions.com>
2021-03-08 13:08:41 -0500

    Added acu_dict.py

---
+++ acu/acu_dict.py
@@ -0,0 +1,45 @@
+acu_var_dict = {
+"space_temp":{"id":"1", "name":"spaceTemp", "value":"74"},
+"return_temp":{"id":"2", "name":"returnTemp", "value":"55"},
+}
+
+#setattr(self, key, Var(name=value["name"], value=value["value"], id=value["id"]))
+
+
+
+temp = {"space_temp":{"id":"1", "name":"spaceTemp", "description":"Space Temperature", "value":"74", "units":"degF", "last_change":"124", "log_enable":1}}
+
+
+
+
+
+
+"""
+
+id
+name
+description
+value
+units
+last_change=60
+log_enable
+log = {"time":value}
+
+
+
+on_time = point = on and last_change > on_time
+stable = last_change > stable
+on_time
+off_time
+stable
+"""
+
+
+
+# change_time
+# ME.AV1_Description.value
+# ME.AV1_Present_Value.value
+# ME.AV1_Units.value
+# ME.AV1_Input_Type.value
+# ME.AV1_Configuration.value
+# ME.AV1
